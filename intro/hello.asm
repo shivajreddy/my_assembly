9bcef0e2d1d53e3df99daba20e1517e1a4d9072b
shiva <smbp@shiva.local>
2025-01-18 17:19:32 -0500

    .

--- intro/hello.asm
+++ intro/hello.asm
@@ -1,21 +1,12 @@
-; hello.asm (64-bit Linux NASM)
+;---------------------------------------------------------------------
+; Writes "Hello World" to the console using only system calls.
+; Runs on 64-bit Linux only
+; To assemble and run:
+; $ nasm -fel64 hello.asm && ld hello.o && ./a.out
+; ---------------------------------------------------------------------
 
-section .data
-    msg     db  "Hello, world!", 0xa  ; 0xa = newline
-    len     equ $ - msg             ; length of the message
 
-section .text
-    global _start
+  global _start
 
-_start:
-    ; syscall: write(STDOUT=1, &msg, len)
-    mov rax, 1       ; system call number for sys_write
-    mov rdi, 1       ; file descriptor 1 (stdout)
-    mov rsi, msg     ; address of our string
-    mov rdx, len     ; length of our string
-    syscall
 
-    ; syscall: exit(0)
-    mov rax, 60      ; system call number for sys_exit
-    xor rdi, rdi     ; exit code 0
-    syscall
+
