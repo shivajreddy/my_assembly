1b29f09c4e82b09063db571eff186c3ed509427a
shiva <sreddy@hey.com>
2025-02-07 07:58:17 -0500

    .

--- x86_Assembly_with_NASM/data.asm
+++ x86_Assembly_with_NASM/data.asm
@@ -6,8 +6,8 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 section .data
-  num  DB  77
-  num2 DB  88
+  num  DB  1
+  num2 DB  2
   ; num => name of the variable
   ; DD => size, DD means double words which is 32 bits(4 bytes)
   ; 99 => value
@@ -16,8 +16,10 @@ section .text
 global _start
 
 _start:
-        MOV ebx,[num]
-        MOV ecx,[num2]
+        ; MOV ebx,[num]
+        ; MOV ecx,[num2]
+        MOV bl,[num]
+        MOV cl,[num2]
 
         MOV eax,1
         INT 80h
