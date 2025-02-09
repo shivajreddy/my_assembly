ab168986aded11874653cec4435ca22f06a018d2
shiva <sreddy@hey.com>
2025-02-09 10:28:10 -0500

    .

--- x86_Assembly_with_NASM/32.asm
+++ x86_Assembly_with_NASM/32.asm
@@ -1,24 +1,20 @@
-; /usr/local/bin/nasm -f macho 32.asm && ld -macosx_version_min 10.7.0 -o 32 32.o && ./32
-
-global start
+section .data
+    ; Define a null-terminated string
+    message db 'Hello, macOS!', 0x0A  ; 0x0A is the newline character
+    message_len equ $ - message       ; Calculate the length of the message
 
 section .text
-start:
-    push    dword msg.len
-    push    dword msg
-    push    dword 1
-    mov     eax, 4
-    sub     esp, 4
-    int     0x80
-    add     esp, 16
+    global _main                      ; Entry point for macOS
 
-    push    dword 0
-    mov     eax, 1
-    sub     esp, 12
-    int     0x80
+_main:
+    ; Write the message to stdout
+    mov eax, 4                        ; syscall: write
+    mov ebx, 1                        ; file descriptor: stdout
+    mov ecx, message                  ; pointer to the message
+    mov edx, message_len              ; length of the message
+    int 0x80                          ; invoke the system call
 
-section .data
-
-msg:    db      "Hello, world!", 10
-.len:   equ     $ - msg
-
+    ; Exit the program
+    mov eax, 1                        ; syscall: exit
+    mov ebx, 0                        ; exit code: 0
+    int 0x80                          ; invoke the system call
